020feff3378ab27f4b7ccdc65106e7b5fc130880
kxrweng <kwlimlearns@gmail.com>
2025-03-31 16:33:27 +0800

    fix: fix eslint errors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import { express } from 'express'
-const app = express()
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { express } from 'express';
+const app = express();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-})
+});
